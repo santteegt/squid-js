5a1016d1fbde7160176367e9709780f925d4df76
Sebastian Gerske <sebastian.gerske@oceanprotocol.com>
2018-12-08 21:51:37 +0100

    fixed codacy

--- src/examples/InitializeAgreement.ts
+++ src/examples/InitializeAgreement.ts
@@ -27,7 +27,7 @@ import * as config from "./config.json"
             // tslint:disable-next-line
             workExample: "stationId,latitude,longitude,datetime,temperature,humidity423432fsd,51.509865,-0.118092,2011-01-01T10:55:11+00:00,7.2,68",
             contentUrls: [
-                "https://testocnfiles.blob.core.windows.net/testfiles/testzkp.pdf"
+                "https://testocnfiles.blob.core.windows.net/testfiles/testzkp.pdf",
             ],
             links: [
                 {sample1: "http://data.ceda.ac.uk/badc/ukcp09/data/gridded-land-obs/gridded-land-obs-daily/"},
