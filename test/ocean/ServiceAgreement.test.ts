1724412dd758e2aba357f081e29901fb738a0cbc
Sebastian Gerske <sebastian.gerske@gmail.com>
2018-12-14 15:30:23 +0100

    adapted unit tets to the code

--- test/ocean/ServiceAgreement.test.ts
+++ test/ocean/ServiceAgreement.test.ts
@@ -23,7 +23,7 @@ let consumerAccount: Account
 let accessService: Service
 let metaDataService: Service
 
-const assetId: string = IdGenerator.generateId()
+const assetId: string = IdGenerator.generatePrefixedId()
 
 describe("ServiceAgreement", () => {
 
