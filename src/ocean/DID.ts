89dbc1053246ca1d61e7773c837cd4c6feaaa88b
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2019-01-09 16:17:23 +0100

    added comments to main Ocean class #79

--- src/ocean/DID.ts
+++ src/ocean/DID.ts
@@ -2,8 +2,16 @@ import IdGenerator from "./IdGenerator"
 
 const prefix = "did:op:"
 
+/**
+ * Decentralized ID.
+ */
 export default class DID {
 
+    /**
+     * Parses a DID from a string.
+     * @param  {string} didString DID in string.
+     * @return {DID}
+     */
     public static parse(didString: string): DID {
         let did: DID
         if (didString.startsWith(prefix)) {
@@ -20,21 +28,36 @@ export default class DID {
         return did
     }
 
+    /**
+     * Returns a new DID.
+     * @return {DID}
+     */
     public static generate(): DID {
         return new DID(IdGenerator.generateId())
     }
 
+    /**
+     * ID.
+     * @type {string}
+     */
     private id: string
 
     private constructor(id: string) {
-
         this.id = id
     }
 
+    /**
+     * Returns the DID.
+     * @return {string}
+     */
     public getDid(): string {
         return `${prefix}${this.id}`
     }
 
+    /**
+     * Returns the ID.
+     * @return {string}
+     */
     public getId(): string {
         return this.id
     }
